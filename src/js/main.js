d74431672db25b5f1f76e9b7d6d2c4e9b74bc256
Szymon <shalerz2003@gmail.com>
2023-09-26 13:16:42 +0200

    problem with mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,28 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav-mobile')
+const footerYear = document.querySelector('.footer__year')
 
-const handleNav = () => {
-    navMobile.classList.toggle('nav-mobile--active')
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 
+const handleNav = () => {
+	document.body.classList.toggle('sticky-body')
+	navMobile.classList.toggle('nav-mobile--active')
+
+	if (document.body.classList.contains('sticky-body')) {
+		burgerBtn.classList.toggle('is-active')
+	} else {
+		burgerBtn.classList.remove('is-active')
+	}
+}
+
+navMobile.querySelectorAll('a.nav__link').forEach(el =>
+	el.addEventListener('click', () => {
+		navMobile.classList.remove('nav-mobile--active')
+	})
+)
+
+handleCurrentYear()
 burgerBtn.addEventListener('click', handleNav)
