7aa4c1c1e686ab140f3111d97de51337c756d4aa
Szymon <shalerz2003@gmail.com>
2023-09-26 17:31:32 +0200

    Fixed nav and burger icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav-mobile')
+const navDesktop = document.querySelector('.nav-desktop')
 const footerYear = document.querySelector('.footer__year')
+
+const menuItems = navDesktop.querySelectorAll('a.nav__link')
+const scrollSpySections = document.querySelectorAll('section.wrapper')
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -17,8 +21,36 @@ navMobile.querySelectorAll('a.nav__link').forEach(el =>
 	el.addEventListener('click', () => {
 		navMobile.classList.remove('nav-mobile--active')
 		burgerBtn.classList.remove('is-active')
+		document.body.classList.remove('sticky-body')
 	})
 )
 
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = []
+
+		scrollSpySections.forEach(section => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 103) {
+				sections.push(section)
+
+				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
+
+				menuItems.forEach(item => item.classList.remove('active'))
+
+				activeSection.classList.add('active')
+			}
+
+			// if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+			//     const lastSection = document.querySelector('a:last-of-type')
+
+			//     menuItems.forEach(item => item.classList.remove('active'))
+
+			//     lastSection.classList.add('active')
+			// }
+		})
+	}
+}
+
 handleCurrentYear()
 burgerBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleScrollSpy)
