d186f087da49a2261923a5bcfd13c5ca18a1100a
Szymon <shalerz2003@gmail.com>
2023-10-02 11:11:30 +0200

    Error in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav-mobile')
 const navDesktop = document.querySelector('.nav-desktop')
+
 const footerYear = document.querySelector('.footer__year')
+
 const menuItems = navDesktop.querySelectorAll('a.nav__link')
 const scrollSpySections = document.querySelectorAll('.section')
+
 const cookieBox = document.querySelector('.cookie-box')
 const cookieBtn = document.querySelector('.cookie-btn')
+
+const userName = document.querySelector('#username')
+const surName = document.querySelector('#surname')
+const email = document.querySelector('#email')
+const textMsg = document.querySelector('#textMsg')
+const clearBtn = document.querySelector('.clear')
+const sendBtn = document.querySelector('.send')
+const popup = document.querySelector('.popup')
+const closePopupBtn = document.querySelector('.close')
 
 // ----------------------------------------
 
@@ -39,7 +51,6 @@ const handleScrollSpy = () => {
 				sections.push(section)
 
 				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
-				console.log(activeSection)
 
 				menuItems.forEach(item => item.classList.remove('active'))
 
@@ -60,14 +71,103 @@ const showCookie = () => {
 }
 
 const handleCookieBox = () => {
-	localStorage.setItem('cookie','true')
+	localStorage.setItem('cookie', 'true')
 	cookieBox.classList.add('hide')
 }
+
+// ----------------------------------------
+
+const inputArr = [userName, surName, email, textMsg]
+
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
+
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
+
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+const checkLenght = (input, min) => {
+	const name = input.previousElementSibling.textContent.slice(0, -1)
+
+	if (input.value.length < min) {
+		showError(input, `${name} jest nieprawidłowe.`)
+	}
+}
+
+const checkEmail = email => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'Adres e-mail jest niepoprawny.')
+	}
+}
+
+const checkMsg = textMsg => {
+	if (textMsg.value === '') {
+		showError(textMsg, 'Wpisz treść wiadomości.')
+	} else {
+		clearError(textMsg)
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
+}
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault()
+
+	checkForm(inputArr)
+	checkLenght(userName, 2)
+	checkLenght(surName, 2)
+	checkEmail(email)
+	checkMsg(textMsg)
+	checkErrors()
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault()
+
+	inputArr.forEach(el => {
+		el.value = ''
+		clearError(el)
+	})
+})
 
 // ----------------------------------------
 
 handleCurrentYear()
 burgerBtn.addEventListener('click', handleNav)
 window.addEventListener('scroll', handleScrollSpy)
-cookieBox.addEventListener('click', handleCookieBox)
+cookieBtn.addEventListener('click', handleCookieBox)
 showCookie()
